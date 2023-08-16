a8e9a01dcb653fa3bdc82d04ac19c65f723d60df
Ruan Serra Azul <ruan.4321.pablo@gmail.com>
2023-08-15 21:26:29 -0300

    Adicionada integração com OpenAI

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
+use App\Models\SalesCommission;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,8 +29,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+    Route::resource('/clients', ClientController::class);
+
+    Route::get('/chart', function () {
+        $fields = implode(',', SalesCommission::getColumns());
+
+        $question = 'Gere um gráfico das vendas por empresa no eixo y ao longo dos últimos 5 anos';
+
+        $config =  OpenAI::completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => "Considerando a lista de campos ($fields), gere uma configuração json do Vega-lite v5 (sem campo de dados e com descrição) que atenda o seguinte pedido $question. Resposta:",
+            'max_tokens' => 1500
+        ])->choices[0]->text;
+
+        return $config;
+    });
 });
 
-Route::resource('/clients', ClientController::class);
+
+
 
 require __DIR__ . '/auth.php';
